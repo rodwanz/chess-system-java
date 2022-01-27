c824a67d6758ac278f0b0bb3726e89306c9e9716
Rodrigo Wanzeler <wanzervai@gmail.com>
2022-01-27 09:10:39 -0300

    Special move - Promotion

--- src/application/ProgramChess.java
+++ src/application/ProgramChess.java
@@ -37,7 +37,12 @@ public class ProgramChess {
 				
 				if(capturedPiece != null) {
 					captured.add(capturedPiece);
-				}				
+				}
+				if(chessMatch.getPromoted() != null) {
+					System.out.print("Enter piece for promotion (B, N, R, Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
 			}
 			catch(ChessException e) {
 				System.out.println(e.getMessage());
